fb4f5d3db25b4b916c2b559411ae4dbd541c1d4c
Sergio <tinyminion83@gmail.com>
2025-12-01 19:53:13 -0600

    Updated app.py to include render production link

--- backend/app.py
+++ backend/app.py
@@ -35,7 +35,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000", 
     "http://127.0.0.1:3000",
-    
+    "https://full-circle-theta.vercel.app"
 ]
 
 # CORS middleware to allow requests from the frontend
