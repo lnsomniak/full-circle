2ed794174b7cad1f0d7069a03b936c7064e4a7bf
Sergio <tinyminion83@gmail.com>
2025-12-05 02:18:51 -0600

    Added Vercel Analytics for real time data collection

--- frontend/full_circle_ui/app/layout.tsx
+++ frontend/full_circle_ui/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpotifyProvider } from "@/lib/spotifycontext";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SpotifyProvider>
           {children}
         </SpotifyProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
