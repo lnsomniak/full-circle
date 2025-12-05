e9b788558a2ff993d6ef5520ff25f3a77b3e626e
Sergio <tinyminion83@gmail.com>
2025-12-05 02:26:42 -0600

    Hopefuly a working analytics component

--- frontend/full_circle_ui/app/layout.tsx
+++ frontend/full_circle_ui/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SpotifyProvider } from "@/lib/spotifycontext";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
